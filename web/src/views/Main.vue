5951c8dae7f16bc29c497463c00135f59a54e545
winston <944241669@qq.com>
2021-09-27 23:41:55 +0800

    初始化web

---
+++ web/src/views/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="topbar bg-black py-2 px-4">
+        
+    </div>
+    <div class="nav">
+        nav
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {};
+</script>
