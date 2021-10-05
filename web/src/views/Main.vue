0effd92b93fb1122483d32d182712ba2ec1a2f87
winston <944241669@qq.com>
2021-10-05 21:37:36 +0800

    顶部导航栏制作

--- web/src/views/Main.vue
+++ web/src/views/Main.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
-    <div class="topbar bg-black py-2 px-4">
-        
+    <div class="topbar bg-black py-2 px-4 d-flex ai-center">
+      <img src="../assets/logo.png" height="30" alt="" />
+      <div class="px-2 flex-1">
+        <div class="text-white">王者荣耀</div>
+        <div class="text-grey-1 fs-xss">团队成就更多</div>
+      </div>
+      <button type="button" class="btn bg-primary">立即下载</button>
     </div>
-    <div class="nav">
-        nav
+    <div class="bg-primary pb-2 pt-3">
+      <div class="nav d-flex text-white pb-1 jc-around">
+        <div class="nav-item active">
+          <router-link to="/" tag="div" class="nav-link">首页</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link to="/" tag="div" class="nav-link">攻略中心</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link to="/" tag="div" class="nav-link">赛事中心</router-link>
+        </div>
+      </div>
     </div>
     <router-view></router-view>
   </div>
