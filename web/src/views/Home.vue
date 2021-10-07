2dcd1b29783f6b3c7055c3ceb5c49abd2cfc888d
winston <944241669@qq.com>
2021-10-07 22:26:50 +0800

    轮播图实现

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,11 +1,58 @@
 <template>
   <div>
-    首页ddd
+    <swiper :options="swiperOption">
+      <swiper-slide>
+        <img
+          class="w-100"
+          src="https://ossweb-img.qq.com/upload/adw/image/194/20211004/ceeda15ef5f83463a4e9fedaee727607.jpeg"
+          alt=""
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <img
+          class="w-100"
+          src="https://ossweb-img.qq.com/upload/adw/image/194/20211004/ceeda15ef5f83463a4e9fedaee727607.jpeg"
+          alt=""
+        />
+      </swiper-slide>
+      <swiper-slide>
+        <img
+          class="w-100"
+          src="https://ossweb-img.qq.com/upload/adw/image/194/20211004/ceeda15ef5f83463a4e9fedaee727607.jpeg"
+          alt=""
+        />
+      </swiper-slide>
+      <div
+        class="swiper-pagination pagination-home text-right px-3 pb-2"
+        slot="pagination"
+      ></div>
+    </swiper>
   </div>
 </template>
 
 <script>
 export default {
-  
-}
+  data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: ".pagination-home",
+        },
+      },
+    };
+  },
+};
 </script>
+<style lang="scss">
+@import "../assets/scss/_variables.scss";
+.pagination-home {
+  .swiper-pagination-bullet {
+    border-radius: 0.1538rem;
+    opacity: 1;
+    background: #fff;
+    &.swiper-pagination-bullet-active {
+      background: map-get($colors, "info");
+    }
+  }
+}
+</style>
