86e7a56cb3cff7fb8458d396b66cf2508d178f53
winston <944241669@qq.com>
2021-10-16 21:42:02 +0800

    精灵图片模块完成

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- end of swiper -->
     <swiper :options="swiperOption">
       <swiper-slide>
         <img
@@ -27,6 +28,19 @@
         slot="pagination"
       ></div>
     </swiper>
+    <!-- end of swiper -->
+    <div class="nav-icons text-center pt-3 text-dark-1 bg-white mt-3">
+      <div class="d-flex flex-wrap">
+        <div class="nav-item mb-3" v-for="n in 10" :key="n">
+          <i class="sprite sprite-news"></i>
+          <div class="py-2">爆料站</div>
+        </div>
+      </div>
+      <div class="fs-sm bg-light py-2">
+        <i class="sprite sprite-arrow mr-1"></i>
+        <span>收起</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -53,6 +67,32 @@ export default {
     &.swiper-pagination-bullet-active {
       background: map-get($colors, "info");
     }
+  }
+}
+.nav-icons {
+  border-top: 1px solid $border-color;
+  border-bottom: 1px solid $border-color;
+  .nav-item {
+    width: 25%;
+    border-left: 1px solid $border-color;
+    &:nth-child(4n + 1) {
+      border: none;
+    }
+  }
+}
+.sprite {
+  background: url(../assets/index.png) no-repeat 0 0;
+  background-size: 28.8462rem;
+  display: inline-block;
+  &.sprite-news {
+    width: 1.7692rem;
+    height: 1.5385rem;
+    background-position: -224px -67px;
+  }
+  &.sprite-arrow {
+    width: 0.7692rem;
+    height: 0.7692rem;
+    background-position: -141px -232px;
   }
 }
 </style>
